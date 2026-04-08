3c28b1808b9768b9e5247138d19bc8d41a984b6f
Raushan kumar <raushan9263657373@gmail.com>
2026-04-09 01:21:05 +0530

    Add prompt for user input in Stringbilderjava

--- Stringbilderjava.java
+++ Stringbilderjava.java
@@ -1,6 +1,8 @@
 public class Stringbilderjava {
 
   public static void main(String[] args) {
+
+    System.out.println("Enter a String");
     
     String s=new String("Raushan");
     StringBuilder sb=new StringBuilder("Raushan");
